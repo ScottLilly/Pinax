de5596e4ec87c45d18785c1d2882e758afb12f64
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2022-05-17 19:36:03 -0500

    Files all .sln and .csproj files under a passed-in disk path

---
+++ Test.Pinax/Services/TestDiskService.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Linq;
+using Pinax.Models;
+using Pinax.Services;
+using Xunit;
+
+namespace Test.Pinax.Services;
+
+public class TestDiskService
+{
+    [Fact]
+    public void Test_DiskSearch()
+    {
+        var solutions =
+            DiskService.GetSolutions(@"e:\MyPublicProjects\ActivityTracker");
+
+        Assert.Equal(1, solutions.Count);
+        Assert.Equal(5, solutions.Sum(s => s.Projects.Count));
+    }
+
+    [Fact]
+    public void Test_DiskSearchTopLevel()
+    {
+        var solutions =
+            DiskService.GetSolutions(@"e:\MyPublicProjects");
+
+        Assert.Equal(20, solutions.Count);
+        Assert.Equal(70, solutions.Sum(s => s.Projects.Count));
+    }
+}
