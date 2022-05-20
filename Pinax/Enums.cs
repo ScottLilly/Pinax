009858b948eec2412c1702791eceb7ee7ecf51cb
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2022-05-19 20:08:55 -0500

    Get .NET type for projects

--- Pinax/Enums.cs
+++ Pinax/Enums.cs
@@ -2,6 +2,15 @@
 
 public static class Enums
 {
+    public enum DotNetType
+    {
+        Unknown,
+        Standard,
+        Framework,
+        Core,
+        DotNet
+    }
+
     public enum Source
     {
         Disk,
