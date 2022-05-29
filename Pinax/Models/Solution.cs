4258070abc432f814b60426e57cb4cada6b026ba
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2022-05-29 14:36:37 -0500

    Only show solutions with outdated projects if user passes in "--outdated" parameter

--- Pinax/Models/Solution.cs
+++ Pinax/Models/Solution.cs
@@ -6,6 +6,9 @@ public class Solution
 {
     public string Name { get; set; }
 
+    public bool HasOutdatedSolutions(Enums.WarningLevel warningLevel) =>
+        Projects.Any(p => p.IsOutdated(warningLevel));
+
     public List<DotNetProject> Projects { get; } =
         new List<DotNetProject>();
 }
