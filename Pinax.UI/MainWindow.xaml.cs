7c100dab6a913c548fcce9873ac27baf3c831017
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2022-10-02 15:58:27 -0500

    Initial windows

---
+++ Pinax.UI/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using Pinax.UI.Windows;
+
+namespace Pinax.UI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Help_OnClick(object sender, RoutedEventArgs e)
+        {
+            Help helpWindow = new Help();
+            helpWindow.Owner = this;
+
+            helpWindow.Show();
+        }
+
+        private void About_OnClick(object sender, RoutedEventArgs e)
+        {
+            About aboutWindow = new About();
+            aboutWindow.Owner = this;
+
+            aboutWindow.ShowDialog();
+        }
+    }
+}
